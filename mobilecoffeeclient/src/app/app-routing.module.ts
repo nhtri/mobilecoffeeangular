25fb9ac908845da7e9687f6fa03839f378bd3a70
Tri Nguyen <tri.nguyen1@tyme.com>
2021-04-01 06:22:15 +0700

    updated

--- mobilecoffeeclient/src/app/app-routing.module.ts
+++ mobilecoffeeclient/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SectionPriceEachComponent } from './section-price-each/section-price-each.component';
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'detail', component: ProductDetailComponent },{ path: '', component: SectionPriceEachComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
