423699197b4e437d49fae443c8e1c231a20e1d4a
Tri Nguyen <tri.nguyen1@tyme.com>
2021-04-28 05:52:24 +0700

    updated

--- mobilecoffeeclient/src/app/product-detail/product-detail.component.ts
+++ mobilecoffeeclient/src/app/product-detail/product-detail.component.ts
@@ -3,7 +3,7 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NetworkserviceService } from '../services/networkservice.service';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import { Url } from 'url';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Pipe({ name: 'safety' })
 export class SafePipe implements PipeTransform {
@@ -25,7 +25,7 @@ export class ProductDetailComponent implements OnInit {
   closeResult: string;
 
   constructor(private sanitizer: DomSanitizer,
-    private networkserviceService: NetworkserviceService, private modalService: NgbModal) { }
+    private networkserviceService: NetworkserviceService, private modalService: NgbModal, private route: ActivatedRoute) { }
 
 
   showModal: boolean;
@@ -34,6 +34,7 @@ export class ProductDetailComponent implements OnInit {
   showModal3: boolean;
   showModal4: boolean;
   showModal5: boolean;
+  showModal6: boolean;
   show() {
     this.showModal = true; // Show-Hide Modal Check    
   }
@@ -78,6 +79,15 @@ export class ProductDetailComponent implements OnInit {
   hide5() {
     this.showModal5 = false;
   }
+
+  show6() {
+    this.showModal6 = true; // Show-Hide Modal Check    
+  }
+  //Bootstrap Modal Close event
+  hide6() {
+    this.showModal6 = false;
+  }
+
   trustedDashboardUrl : SafeResourceUrl;
   data: any
   video:String
@@ -85,11 +95,20 @@ export class ProductDetailComponent implements OnInit {
     if (window.history.state.category != null) {
       this.data = window.history.state;
       localStorage.setItem('data', JSON.stringify(this.data))
-      this.video = this.data.video
+      this.video = "http://tonylemobile.com/assets/images/"+this.data.video
+      console.log("editData",this.data,this.video)
     }
     else {
       this.data = JSON.parse(localStorage.getItem('data'))
     }
+
+    this.route.queryParams
+      .subscribe(params => {
+        console.log('params',params.id[0]); // { order: "popular" }
+
+  
+      }
+    );
 
   }
 
